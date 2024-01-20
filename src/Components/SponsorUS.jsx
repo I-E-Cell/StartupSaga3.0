f0d0b905185a130ba9289220ccd840ad25db4365
Tanishksoam <soamtanishk@gmail.com>
2024-01-20 14:46:02 +0530

    somewhat reponsive

--- src/Components/SponsorUS.jsx
+++ src/Components/SponsorUS.jsx
@@ -6,9 +6,9 @@ import subtitle from "../assets/sponsorus/subtitle.png";
 const SponsorUS = () => {
   return (
     <div className="flex flex-col gap-8  w-screen  px-1 justify-center items-center overflow-hidden">
-      <div className="flex flex-col gap-8 w-[90%]  px-1 justify-center items-center overflow-hidden">
+      <div className="flex flex-col gap-8  lg:w-[90%] md:w-[130vw] w-[150vw] px-1 justify-center items-center overflow-hidden">
         <div className=" w-[90%] flex items-center justify-center  max-w-[1400px] rounded-3xl overflow-hidden">
-          <img src={title} className=" w-4/5" alt="" />
+          <img src={title} className=" w-3/5" alt="" />
         </div>
         <div className="w-[95%] max-w-[1400px]  bg-orange-200 rounded-3xl overflow-hidden relative">
           <img
@@ -17,11 +17,11 @@ const SponsorUS = () => {
             alt=""
           />
           <img src={background} className="w-full object-cover z-10" alt="" />
-          <div className="w-[80%] h-[20%] absolute left-[10%] top-[68%] flex flex-col gap-2 justify-between items-center">
+          <div className="w-[80%] h-[20%] absolute left-[10%] sm:top-[68%] top-[64%] flex flex-col gap-2 justify-between items-center">
             <h2 className="w-full text-center font-bodyFont text-[120%] font-semibold ">
               Fill the form to reach out to us!!
             </h2>
-            <div className="w-full grid grid-cols-2 gap-2 text-[80%] px-4">
+            <div className="w-full grid grid-cols-2 gap-1 sm:gap-2 text-[80%] px-20 md:px-4">
               <div class="flex w-full items-center justify-center">
                 <div class="relative w-full">
                   <input
