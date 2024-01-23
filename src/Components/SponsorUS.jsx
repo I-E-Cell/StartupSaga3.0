e7a3f8f41eac1270b86135bcf6c37401450213c1
Tanishksoam <soamtanishk@gmail.com>
2024-01-23 23:51:37 +0530

    sponsorUS design change

--- src/Components/SponsorUS.jsx
+++ src/Components/SponsorUS.jsx
@@ -6,7 +6,7 @@ import subtitle from "../assets/sponsorus/subtitle.png";
 const SponsorUS = () => {
   return (
     <div className="flex flex-col gap-8  w-screen  px-1 justify-center items-center overflow-hidden">
-      <div className="flex flex-col gap-8  lg:w-[90%] md:w-[130vw] w-[150vw] px-1 justify-center items-center overflow-hidden">
+      <div className="flex flex-col gap-4  lg:w-[98%] md:w-[130vw] w-[150vw] px-1 justify-center items-center overflow-hidden">
         <div className=" w-[90%] flex items-center justify-center  max-w-[1400px] rounded-3xl overflow-hidden">
           <img src={title} className=" w-3/5" alt="" />
         </div>
@@ -17,77 +17,69 @@ const SponsorUS = () => {
             alt=""
           />
           <img src={background} className="w-full object-cover z-10" alt="" />
-          <div className="w-[80%] h-[20%] absolute left-[10%] sm:top-[68%] top-[64%] flex flex-col gap-2 justify-between items-center">
-            <h2 className="w-full text-center font-bodyFont text-[80%] sm:text-[120%] font-semibold ">
+          <div className="w-[80%] h-auto py-2 absolute left-[10%] top-[23%] flex flex-col gap-4 justify-between items-center bg-[#fcb87c] backdrop-blur-sm bg-opacity-80  border-solid border-2 border-[#fb9842] rounded-2xl ">
+            <h2 className="w-full text-center font-bodyFont text-[120%] lg:text-[200%] font-semibold ">
               Fill the form to reach out to us!!
             </h2>
-            <div className="w-full grid grid-cols-2 gap-1 sm:gap-2 text-[50%] sm:text-[80%] px-20 md:px-4">
-              <div class="flex w-full items-center justify-center">
-                <div class="relative w-full">
+            <div className="w-full flex flex-col  gap-6 text-[100%] text-[#4e171f] font-medium px-20 md:px-4">
+              <div className="flex w-full items-center justify-center">
+                <div className="input-container">
                   <input
-                    id="companyName"
-                    name="companyName"
+                    placeholder="Name"
+                    className="input-field"
+                    name="name"
                     type="text"
-                    class="border-b-2 border-[#fcb87c] w-full py-1 focus:border-b-2 focus:border-blue-700 transition-colors focus:outline-none peer bg-inherit"
                   />
-                  <label
-                    for="companyName"
-                    class="absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 transition-all peer-focus:text-blue-700"
-                  >
+                  <label htmlFor="input-field" className="input-label">
                     Company Name
                   </label>
+                  <span className="input-highlight"></span>
                 </div>
               </div>
-              <div class="flex w-full items-center justify-center">
-                <div class="relative w-full">
+              <div className="flex w-full items-center justify-center">
+                <div className="input-container">
                   <input
-                    id="contact"
+                    placeholder="Contact No."
+                    className="input-field"
                     name="contact"
                     type="text"
-                    class="border-b-2 border-[#fcb87c] w-full py-1 focus:border-b-2 focus:border-blue-700 transition-colors focus:outline-none peer bg-inherit"
                   />
-                  <label
-                    for="contact"
-                    class="absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 transition-all peer-focus:text-blue-700"
-                  >
-                    Contact no.
+                  <label htmlFor="input-field" className="input-label">
+                    Contact No.
                   </label>
+                  <span className="input-highlight"></span>
                 </div>
               </div>
-              <div class="flex w-full items-center justify-center">
-                <div class="relative w-full">
+              <div className="flex w-full items-center justify-center">
+                <div className="input-container">
                   <input
-                    id="email"
+                    placeholder="Email"
+                    className="input-field"
                     name="email"
                     type="text"
-                    class="border-b-2 border-[#fcb87c]  w-full py-1 focus:border-b-2 focus:border-blue-700 transition-colors focus:outline-none peer bg-inherit"
                   />
-                  <label
-                    for="email"
-                    class="absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 transition-all peer-focus:text-blue-700"
-                  >
+                  <label htmlFor="input-field" className="input-label">
                     Email
                   </label>
+                  <span className="input-highlight"></span>
                 </div>
               </div>
-              <div class="flex w-full items-center justify-center">
-                <div class="relative w-full">
+              <div className="flex w-full items-center justify-center">
+                <div className="input-container">
                   <input
-                    id="message"
+                    placeholder="Message"
+                    className="input-field"
                     name="message"
                     type="text"
-                    class="border-b-2 border-[#fcb87c] w-full py-1 focus:border-b-2 focus:border-blue-700 transition-colors focus:outline-none peer bg-inherit"
                   />
-                  <label
-                    for="message"
-                    class="absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 transition-all peer-focus:text-blue-700"
-                  >
+                  <label htmlFor="input-field" className="input-label">
                     Message
                   </label>
+                  <span className="input-highlight"></span>
                 </div>
               </div>
               <div className=" w-full flex justify-center col-span-2 px-8">
-                <button className="bg-[#FC8824] rounded-xl relative h-[1rem] sm:h-[2rem] w-[40%] my-1 text-white">
+                <button className="bg-[#FC8824] rounded-xl relative h-[2rem] w-[40%] sm:my-1 text-white">
                   Submit
                 </button>
               </div>
