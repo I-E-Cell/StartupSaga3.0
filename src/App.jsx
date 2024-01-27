445bc420e95ecc6a82c68f649cc5a9bf4dca11bb
Gulshan <gulshansinghhhh@gmail.com>
2024-01-27 16:22:57 +0530

    added timeline

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Home from "./Components/Home"
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import FAQ from "./Components/FAQ";
 import { About } from "./Components/About";
+import Events from "./Components/Events";
+
 
 function Landing() {
   return (
     <div>
       <Nav />
+
       <Home />
 
       <About />
+      <Events />
       <FAQ />
-
       
+
     </div>
   );
 }
