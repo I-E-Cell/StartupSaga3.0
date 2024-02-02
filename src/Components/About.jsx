25a8f3ed95d30df928a578c37d346c44b33ee98a
Gulshan <gulshansinghhhh@gmail.com>
2024-02-02 16:52:36 +0530

    minor changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -15,103 +15,103 @@ export const About = () => {
     <>
       <div>
         <div className="mt-4  mb-7 md:mb-11 md:mt-14 z-0 ">
-    <div className=" text-center">
-    <h1 className='text-[1.6em] md:text-[3.6em] mx-auto text-center font-extrabold text-[#241303]'>About Us</h1>
+          <div className=" text-center">
+            <h1 className='text-[1.6em] md:text-[3.6em] mx-auto text-center font-extrabold text-[#241303]'>About Us</h1>
 
-<div className='w-[90%] mx-auto flex gap-3 justify-evenly items-center'>
-    <hr className='w-[45%] h-[3px] bg-[#241303] mx-auto' />
-    <GoDotFill size={10} />
-    <GoDotFill size={10} />
-    <GoDotFill size={10} />
-    <hr className='w-[45%] h-[3px] bg-[#241303]  mx-auto' />
-</div>
-</div>
-    </div>
+            <div className='w-[90%] mx-auto flex gap-3 justify-evenly items-center'>
+              <hr className='w-[45%] h-[3px] bg-[#241303] mx-auto' />
+              <GoDotFill size={10} />
+              <GoDotFill size={10} />
+              <GoDotFill size={10} />
+              <hr className='w-[45%] h-[3px] bg-[#241303]  mx-auto' />
+            </div>
+          </div>
+        </div>
         <div className="relative mx-auto flex flex-col gap-5">
           <div className="relative    mx-auto w-[90%] max-w-[72em]  ">
             <div className=" absolute text-center about-text w-[55em]">
-              
-                      <Abouttext/>
-                  
-            
+
+              <Abouttext />
+
+
             </div>
             <img className="w-[100%] hidden md:block " src={about} alt=""></img>
             <img className="w-[100%] md:hidden block" src={abtpn} alt=""></img>
 
             <motion.img
-            initial={{opacity:0,right:-30}}
-            whileInView={{opacity:1,right:30}}
-            transition={{duration:1}}
+              initial={{ opacity: 0, right: -30 }}
+              whileInView={{ opacity: 1, right: 30 }}
+              transition={{ duration: 1 }}
               className="absolute w-[7em] lg:w-[11em] hidden md:block ait"
               src={ait}
               alt=""
             />
             <motion.img
-            initial={{opacity:0,left:-30}}
-            whileInView={{opacity:1,left:30}}
-            transition={{duration:1}}
+              initial={{ opacity: 0, left: -30 }}
+              whileInView={{ opacity: 1, left: 30 }}
+              transition={{ duration: 1 }}
               className="absolute w-[3.3em] lg:w-[6.5em]  hidden md:block horse"
               src={horse}
               alt=""
             />
-            
+
             <motion.img
-            initial={{opacity:0,right:-10}}
-            whileInView={{opacity:1,right:10}}
-            transition={{duration:1}}
-            
-            className="absolute w-[5em] md:hidden block aitpn    " src={ait} alt="" />
-            <motion.img 
-            initial={{opacity:0,left:-10}}
-            whileInView={{opacity:1,left:10}}
-            transition={{duration:1}}
-            
-            className="absolute w-[2.3em] md:hidden block horsepn" src={horse} alt="" />
+              initial={{ opacity: 0, right: -10 }}
+              whileInView={{ opacity: 1, right: 10 }}
+              transition={{ duration: 1 }}
+
+              className="absolute w-[5em] md:hidden block aitpn    " src={ait} alt="" />
+            <motion.img
+              initial={{ opacity: 0, left: -10 }}
+              whileInView={{ opacity: 1, left: 10 }}
+              transition={{ duration: 1 }}
+
+              className="absolute w-[2.3em] md:hidden block horsepn" src={horse} alt="" />
           </div>
           <div className="relative mx-auto w-[90%] max-w-[72em] ">
             <div className="absolute text-center about-text">
-              
-                 <Abouttext1/>    
-                
+
+              <Abouttext1 />
+
             </div>
 
-            <img className="w-[100%] hidden md:block" src={about} alt=""/>
-            <img className="w-[100%] md:hidden block" src={abtpn} alt=""/>
+            <img className="w-[100%] hidden md:block" src={about} alt="" />
+            <img className="w-[100%] md:hidden block" src={abtpn} alt="" />
 
             <motion.img
-              initial={{opacity:0,right:-30}}
-              whileInView={{opacity:1,right:30}}
-              transition={{duration:1}}
+              initial={{ opacity: 0, right: -30 }}
+              whileInView={{ opacity: 1, right: 30 }}
+              transition={{ duration: 1 }}
               className="absolute hidden md:block w-[3.3em] lg:w-[6.5em]  ait"
               src={horse2}
               alt=""
             />
             <motion.img
-            initial={{opacity:0,left:-30}}
-            whileInView={{opacity:1,left:30}}
-            transition={{duration:1}}
+              initial={{ opacity: 0, left: -30 }}
+              whileInView={{ opacity: 1, left: 30 }}
+              transition={{ duration: 1 }}
               className="absolute hidden md:block w-[8em] lg:w-[15em]  ss2"
               src={ss}
               alt=""
             />
             <motion.img
-            initial={{opacity:0,right:-10}}
-            whileInView={{opacity:1,right:10}}
-            transition={{duration:1}}
+              initial={{ opacity: 0, right: -10 }}
+              whileInView={{ opacity: 1, right: 10 }}
+              transition={{ duration: 1 }}
               className="absolute md:hidden block w-[2.3em] horse2 ait "
               src={horse2}
               alt=""
             />
-            <motion.img 
-            initial={{opacity:0,left:-10}}
-            whileInView={{opacity:1,left:10}}
-            transition={{duration:1}}
+            <motion.img
+              initial={{ opacity: 0, left: -10 }}
+              whileInView={{ opacity: 1, left: 10 }}
+              transition={{ duration: 1 }}
 
-            className="absolute md:hidden block w-[7.2em]  ss " src={ss} alt="" />
-           
-            
+              className="absolute md:hidden block w-[7.2em]  ss " src={ss} alt="" />
+
+
           </div>
-          
+
         </div>
       </div>
     </>
--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -6,6 +6,7 @@ import nivesh from '../assets/nivesh.svg'
 import meet from '../assets/meet.svg'
 import escape from '../assets/escape.svg'
 import theme from '../assets/theme.svg'
+import { GoDotFill } from "react-icons/go";
 
 import sharktank from '../assets/sharktank.svg'
 
@@ -29,107 +30,121 @@ const Events = () => {
   };
 
   return (
-    
-    <div id='events' className="max-w-[90%] crack hidden md:flex crack  items-center w-[87em]  relative overflow-hidden crack border-[3px] my-14 mx-auto border-broq md:h-[36em] rounded-2xl bg-gradient-to-b from-[#FCD7B5] to-[#F8EBD5]  ">
-      {/* <img className='absolute z-0  opacity-70 w-[100em] ' src={crack} alt="" /> */}
-      {showModal && <Modal EventName={display} closeModal={closeModal} />}
-      
-      <div className="flex flex-col z-10 gap-8 w-[100%]   items-center">
-        <div className=" w-full flex justify-evenly">
-          <div className=" w-[7.5em] rounded-lg  text-center flex flex-col gap-5">
-            <h2 className="text-lg font-semibold text-center"> Niveshathon</h2>
-            <h3 className=" font-bold">27/03/2024</h3>
-          </div>
-          <div
-            onClick={() => displaymodal("Speaker Session")}
-            onMouseEnter={() => moveTrain({ top: "-1.25em", left: "11%" })}
-            className="w-[7.5em] flex flex-col justify-center items-center rounded-lg h-[7.5em] gap-5  text-[#F0D6B8] border-[1px] border-[#210D00] bg-[#9E4D16]"
-          >
-            <img className='w-[70%] ' src={nivesh} alt="" />
-            <h1 className='font-semibold'>Register Here !</h1>
-          </div>
-          <div className=" w-[7.5em] rounded-lg  text-center flex flex-col gap-5">
-            <h2 className="text-lg font-semibold text-center">
-              Shark Tank AIT
-            </h2>
-            <h3 className=" font-bold">29/03/2024</h3>
-          </div>
-          <div
-            onClick={() => displaymodal("Ecell Meet")}
-            onMouseEnter={() => moveTrain({ top: "-1.25em", left: "42%" })}
-            className="w-[7.5em] flex flex-col justify-center items-center rounded-lg h-[7.5em] gap-5  text-[#F0D6B8] border-[1px] border-[#210D00] bg-[#9E4D16]"
-          >
-            <img className='w-[60%] ' src={meet} alt="" />
-            <h1 className='font-semibold'>Register Here !</h1>
-          </div>
-          <div className=" w-[7.5em] rounded-lg  text-center flex flex-col gap-5">
-            <h2 className="text-lg font-semibold text-center"> Escape Room</h2>
-            <h3 className=" font-bold">30/03/2024</h3>
-          </div>
-          <div
-            onClick={() => displaymodal("Theme-based Hackathon")}
-            onMouseEnter={() => moveTrain({ top: "-1.25em", left: "75%" })}
-            className="w-[7.5em] flex flex-col justify-center items-center rounded-lg h-[7.5em] gap-5  text-[#F0D6B8] border-[1px] border-[#210D00] bg-[#9E4D16]"
-          >
-            <img className='w-[45%] ' src={theme} alt="" />
-            <h1 className='font-semibold'>Register Here !</h1>
+    <>
+
+        <div className=" text-center">
+          <h1 className='text-[1.6em] md:text-[3.6em] mx-auto text-center font-extrabold text-[#241303] mt-14'>Timeline</h1>
+
+          <div className='w-[90%] mx-auto flex gap-3 justify-evenly items-center'>
+            <hr className='w-[45%] h-[3px] bg-[#241303] mx-auto' />
+            <GoDotFill size={10} />
+            <GoDotFill size={10} />
+            <GoDotFill size={10} />
+            <hr className='w-[45%] h-[3px] bg-[#241303]  mx-auto' />
           </div>
         </div>
-        <img className="" src={tracks} alt="" />
-        <div className=" w-full flex justify-evenly">
-          <div
-            onClick={() => displaymodal("Niveshathon")}
-            onMouseEnter={() => moveTrain({ top: "-1.25em", left: "-6%" })}
-            className="w-[7.5em]  flex flex-col justify-center items-center font-semibold gap-5  text-[#F0D6B8] rounded-lg h-[7.5em] border-[1px] border-[#210D00] bg-[#9E4D16]"
-          >
-            <img className='w-[70%] ' src={nivesh} alt="" />
-            <h1 className='font-semibold'>Register Here !</h1>
+
+      <div id='events' className="max-w-[90%] crack hidden md:flex crack  items-center w-[87em]  relative overflow-hidden crack border-[3px] my-9 mx-auto border-broq md:h-[36em] rounded-2xl bg-gradient-to-b from-[#FCD7B5] to-[#F8EBD5]  ">
+        {/* <img className='absolute z-0  opacity-70 w-[100em] ' src={crack} alt="" /> */}
+        {showModal && <Modal EventName={display} closeModal={closeModal} />}
+
+        <div className="flex flex-col z-10 gap-8 w-[100%]   items-center">
+          <div className=" w-full flex justify-evenly">
+            <div className=" w-[7.5em] rounded-lg  text-center flex flex-col gap-5">
+              <h2 className="text-lg font-semibold text-center"> Niveshathon</h2>
+              <h3 className=" font-bold">27/03/2024</h3>
+            </div>
+            <div
+              onClick={() => displaymodal("Speaker Session")}
+              onMouseEnter={() => moveTrain({ top: "-1.25em", left: "11%" })}
+              className="w-[7.5em] flex flex-col justify-center items-center rounded-lg h-[7.5em] gap-5  text-[#F0D6B8] border-[1px] border-[#210D00] bg-[#9E4D16]"
+            >
+              <img className='w-[70%] ' src={nivesh} alt="" />
+              <h1 className='font-semibold'>Register Here !</h1>
+            </div>
+            <div className=" w-[7.5em] rounded-lg  text-center flex flex-col gap-5">
+              <h2 className="text-lg font-semibold text-center">
+                Shark Tank AIT
+              </h2>
+              <h3 className=" font-bold">29/03/2024</h3>
+            </div>
+            <div
+              onClick={() => displaymodal("Ecell Meet")}
+              onMouseEnter={() => moveTrain({ top: "-1.25em", left: "42%" })}
+              className="w-[7.5em] flex flex-col justify-center items-center rounded-lg h-[7.5em] gap-5  text-[#F0D6B8] border-[1px] border-[#210D00] bg-[#9E4D16]"
+            >
+              <img className='w-[60%] ' src={meet} alt="" />
+              <h1 className='font-semibold'>Register Here !</h1>
+            </div>
+            <div className=" w-[7.5em] rounded-lg  text-center flex flex-col gap-5">
+              <h2 className="text-lg font-semibold text-center"> Escape Room</h2>
+              <h3 className=" font-bold">30/03/2024</h3>
+            </div>
+            <div
+              onClick={() => displaymodal("Theme-based Hackathon")}
+              onMouseEnter={() => moveTrain({ top: "-1.25em", left: "75%" })}
+              className="w-[7.5em] flex flex-col justify-center items-center rounded-lg h-[7.5em] gap-5  text-[#F0D6B8] border-[1px] border-[#210D00] bg-[#9E4D16]"
+            >
+              <img className='w-[45%] ' src={theme} alt="" />
+              <h1 className='font-semibold'>Register Here !</h1>
+            </div>
           </div>
-          <div className=" w-[7.5em] rounded-lg  text-center flex flex-col gap-5">
-            <h2 className="text-lg font-semibold text-center">
-              {" "}
-              Speaker Session
-            </h2>
-            <h3 className=" font-bold">28/03/2024</h3>
-          </div>
-          <div
-            onClick={() => displaymodal("Shark Tank AIT")}
-            onMouseEnter={() => moveTrain({ top: "-1.25em", left: "26%" })}
-            className="w-[7.5em] flex flex-col justify-center items-center h-[7.5em] rounded-lg  gap-5  text-[#F0D6B8] border-[1px] border-[#210D00] bg-[#9E4D16]"
-          >
-            <img className='w-[40%] ' src={sharktank} alt="" />
-            <h1 className='font-semibold'>Register Here !</h1>
-          </div>
-          <div className=" w-[7.5em] rounded-lg  text-center flex flex-col gap-5">
-            <h2 className="text-lg font-semibold text-center"> Ecell Meet</h2>
-            <h3 className=" font-bold">29/03/2024</h3>
-          </div>
-          <div
-            onClick={() => displaymodal("Escape Room")}
-            onMouseEnter={() => moveTrain({ top: "-1.25em", left: "58%" })}
-            className="w-[7.5em] flex flex-col justify-center items-center rounded-lg h-[7.5em] gap-5  text-[#F0D6B8] border-[1px] border-[#210D00] bg-[#9E4D16]"
-          >
-            <img className='w-[45%] ' src={escape} alt="" />
-            <h1 className='font-semibold'>Register Here !</h1>
-          </div>
-          <div className=" w-[7.5em] rounded-lg  text-center flex flex-col gap-5">
-            <h2 className="text-lg font-semibold text-center">
-              {" "}
-              Startup Competition 
-            </h2>
-            <h3 className=" font-bold">30/03/2024</h3>
+          <img className="" src={tracks} alt="" />
+          <div className=" w-full flex justify-evenly">
+            <div
+              onClick={() => displaymodal("Niveshathon")}
+              onMouseEnter={() => moveTrain({ top: "-1.25em", left: "-6%" })}
+              className="w-[7.5em]  flex flex-col justify-center items-center font-semibold gap-5  text-[#F0D6B8] rounded-lg h-[7.5em] border-[1px] border-[#210D00] bg-[#9E4D16]"
+            >
+              <img className='w-[70%] ' src={nivesh} alt="" />
+              <h1 className='font-semibold'>Register Here !</h1>
+            </div>
+            <div className=" w-[7.5em] rounded-lg  text-center flex flex-col gap-5">
+              <h2 className="text-lg font-semibold text-center">
+                {" "}
+                Speaker Session
+              </h2>
+              <h3 className=" font-bold">28/03/2024</h3>
+            </div>
+            <div
+              onClick={() => displaymodal("Shark Tank AIT")}
+              onMouseEnter={() => moveTrain({ top: "-1.25em", left: "26%" })}
+              className="w-[7.5em] flex flex-col justify-center items-center h-[7.5em] rounded-lg  gap-5  text-[#F0D6B8] border-[1px] border-[#210D00] bg-[#9E4D16]"
+            >
+              <img className='w-[40%] ' src={sharktank} alt="" />
+              <h1 className='font-semibold'>Register Here !</h1>
+            </div>
+            <div className=" w-[7.5em] rounded-lg  text-center flex flex-col gap-5">
+              <h2 className="text-lg font-semibold text-center"> Ecell Meet</h2>
+              <h3 className=" font-bold">29/03/2024</h3>
+            </div>
+            <div
+              onClick={() => displaymodal("Escape Room")}
+              onMouseEnter={() => moveTrain({ top: "-1.25em", left: "58%" })}
+              className="w-[7.5em] flex flex-col justify-center items-center rounded-lg h-[7.5em] gap-5  text-[#F0D6B8] border-[1px] border-[#210D00] bg-[#9E4D16]"
+            >
+              <img className='w-[45%] ' src={escape} alt="" />
+              <h1 className='font-semibold'>Register Here !</h1>
+            </div>
+            <div className=" w-[7.5em] rounded-lg  text-center flex flex-col gap-5">
+              <h2 className="text-lg font-semibold text-center">
+                {" "}
+                Startup Competition
+              </h2>
+              <h3 className=" font-bold">30/03/2024</h3>
+            </div>
           </div>
         </div>
+        <div className="absolute  z-10  w-[85%] lg:w-full">
+          <img
+            className="imgg w-[16em] relative"
+            style={{ top: trainPosition.top, left: trainPosition.left }}
+            src={train}
+            alt=""
+          />
+        </div>
       </div>
-      <div className="absolute  z-10  w-[85%] lg:w-full">
-        <img
-          className="imgg w-[16em] relative"
-          style={{ top: trainPosition.top, left: trainPosition.left }}
-          src={train}
-          alt=""
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
