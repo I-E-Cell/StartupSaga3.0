456a192b2a606cf94446ab0b48537b5db4531a7b
Mitesh2806 <miteshmaity_22519@aitpune.edu.in>
2024-02-06 01:00:49 +0530

    Changed Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,44 +5,51 @@ import {
   FaFacebook,
   FaTwitter,
   FaYoutube,
+  FaPhone,
 } from "react-icons/fa";
 import { BsArrowUpRight } from "react-icons/bs";
-import ssb from "../assets/ssb.svg";
+import ssb from "../assets/ssnew.svg";
 import fmount1 from "../assets/fmount1.svg";
 import fmount from "../assets/fmount.svg";
 import terrain1 from "../assets/terrain1.svg"
 import terrain2 from "../assets/terrain2.svg"
 import cacright from "../assets/home/cacright.svg";
 import cacleft from "../assets/home/cacleft.svg";
+import horserider from "../assets/horserider.svg"; 
 const Footer = () => {
   return (
-    <div className="relative mt-28">
+    <div className="relative mt-16 md:mt-32 pt-[6em] " id="foot">
+       <img
+              className="w-[4em] md:w-[7em] md:block absolute horserider"
+              src={horserider}
+              alt=""
+            />
       <img
-              className="w-[10em] hidden md:block absolute cacfr"
+              className="w-[10em] -z-20 hidden md:block absolute  cacfr"
               src={cacleft}
               alt=""
             />
       
       <img
-              className="w-[10em] hidden md:block absolute cacf"
+              className="w-[10em]  -z-20 hidden md:block absolute cacf"
               src={cacright}
               alt=""
             />
       <img
-        className=" w-[6em] h-[8em] absolute  left-0 mount-left fmount  "
+        className=" w-[6em] h-[8em] absolute top-0  left-0 mount-left fmount  "
         src={fmount1}
         alt=""
       />
-      <img className="absolute w-[7em] h-[8em] fmount1 " src={fmount} alt="" />
-      <img className="absolute w-[6em] h-[8em] fmount " src={fmount1} alt="" /> 
+      <img className="absolute w-[7em] h-[8em] top-0 right-0 fmount1 " src={fmount} alt="" />
+      <img className="absolute w-[6em] h-[8em] top-0 fmount " src={fmount1} alt="" /> 
 
       <div className="">
         <div className="  bg-[#3A281E] flex justify-center w-screen h-full overflow-x-hidden ">
           <div className=""></div>
 
           <div className="container bg-[#3A281E] max-w-[87em] mx-auto mt-3 ">
-            <div className=" py-4 md:py-8 px-2 md:px-20 flex flex-col md:flex-row justify-between items-center w-full">
-              <div className="mb-6 md:mb-0 relative z-10">
+            <div className=" py-4 md:py-8 px-2 md:px-20 flex flex-col sm:flex-row justify-between items-center w-full">
+              <div className="mb-3 md:mb-0 relative z-10">
                 <div className="flex flex-col justify-center gap-6 items-center relative mt-4 md:mt-8 ">
                   <div className=" flex justify-center items-center">
                     <img
@@ -52,7 +59,7 @@ const Footer = () => {
                     />
                   </div>
 
-                  <div className="flex gap-2 relative top-1    md:flex-row md:items-center items-center">
+                  <div className="flex gap-2 relative   md:flex-row md:items-center items-center">
                     <div className=" p-2 md:p-3 bg-[#FFFFFF] rounded-full items-center">
                       <FaFacebook
                         md:size={25}
@@ -107,17 +114,32 @@ const Footer = () => {
                 
                
               </div>
-              <div className="h-[9em] w-[0.1em] hidden md:block bg-white "></div>
-              <div className="textt text-white mx-auto md:mx-0 relative rounded-xl z-[9999999999] p-2 px-2 border-white border-[1px] top-4 ">
-                <p className="text-xs mb-2  w-[180px] md:w-[300px] ">
+              <div className="h-[9em] w-[0.1em] hidden md:block bg-white  "></div>
+              <div className=" flex-col">
+              <div className=" text-white mx-auto md:mx-0 relative rounded-xl z-[9999999999] p-2 px-2 border-white border-[1px] top-4 mb-4 w-[300px] ">
+                <p className="text-xs mb-2  w-[270px] md:w-[300px]  ">
                   “If you look at history, innovation doesn't come just from
                   giving people incentives; it comes from creating environments
                   where their ideas can connect.“
                 </p>
 
-                <a href="/" className="flex justify-end gap-2 text-[#B8BFFF]">
+                <a href="https://www.aitecell.in/" className="flex justify-end gap-2 text-[#B8BFFF]">
                   I & E Cell <BsArrowUpRight color="#B8BFFF" size={15} />
                 </a>
+              </div>
+              <div className=" flex flex-row md:gap-6 gap-4 pt-3  px-2 justify-center">
+                <div className=" ">
+                  <p className=" text-white "> Ayush Kumar</p>
+                  <p className=" text-white text-[0.9em] "> (Secretary)</p>
+                  <p className="text-white flex items-center gap-1 mt-3 "><FaPhone/>9898XXXXXX</p>
+                  
+                </div>
+                <div className="">
+                <p className="text-white ">Jyoti Yadav</p>
+                <p className="text-white   text-[0.9em] ">(Secretary)</p>
+                  <p className="text-white flex items-center gap-1 mt-3  "><FaPhone/>9898XXXXXX</p>
+                  </div>
+              </div>
               </div>
             </div>
           </div>
