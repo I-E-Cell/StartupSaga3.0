ef84461cf4739e8a3426e85e2b7307f2be7c4ab4
Akayush-17 <aaayushsinghh@gmail.com>
2024-02-17 16:46:13 +0530

    added reg link for nivesathon

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -4,53 +4,59 @@ import { motion } from 'framer-motion'
 
 
 const EventData = [
-    {
+    {   
+        id:1,
         logo: '',
         EventName: 'Niveshathon',
         discription: ' Feel the pulse of the market without risking a dime in Niveshathon: Dive in. Beyond Monopoly money, board games. It is a game where you can trade without any real money with real-time market data. ',
         date: '27 Mar 2024',
-        button: 'www.google.com',
-        prize: "₹20,000",
+        button: 'https://unstop.com/competitions/nivesh-a-thon-army-institute-of-technology-ait-pune-899558',
+        prize: "₹30,000",
     },
     {
+        id:2,
         logo: '',
         EventName: 'Speaker Session',
         discription: 'Discover funding hacks, growth strategies, and get insider tips from our experienced entrepreneurs to ensure a smooth and successful launch that sets you on the path to success',
         date: '28 Mar 2024',
-        button: 'www.google.com',
+        button: '#',
         prize: "₹10,000",
     },
     {
+        id:3,
         logo: '',
         EventName: 'Ecell Meet',
         discription: 'The gathering of key stakeholders of E-cell from the various colleges across Pune to connect is not just a boring party but it’s like engaging in discussions and sharing insights. This gathering is a creative melting pot where like-minded people come to collaborate.',
         date: '29 Mar 2024',
-        button: 'www.google.com',
+        button: '#',
         prize: "₹20,000",
     },
     {
+        id:4,
         logo: '',
         EventName: 'Mini Event',
         discription: 'Get your brain buzzing with mind-bending puzzles, test your agility in high-octane competitions, and ignite your competitive spirit like never before. Stay tuned for updates on our electrifying games and get ready to redefine what it means to be a champion! ',
         date: '29 Mar 2024',
-        button: 'www.google.com',
+        button: '#',
         prize: "₹30,000",
 
     },
     {
+        id:5,
         logo: '',
         EventName: 'NextNiche',
         discription: 'Join us at NextNiche a business hackathon for a transformative experience! In 24 hours come up with an innovative startup with your  unique product and prototype idea. Gain helpful connections, guidance, and rewards to kickstart business journey',
         date: '30 Mar 2024',
-        button: 'www.google.com',
+        button: '#',
         prize: "₹1 lakh",
     },
     {
+        id:6,
         logo: '',
         EventName: 'Shark Tank',
         discription: 'Shark Tank is an inter college event which provides an opportunity for students to showcase their Startup ideas in front of a panel of judges, taking inspiration from the popular TV show Shark Tank. The judges will also invest in the startups if they deem it worthy and the negotiation will happen on the spot.   ',
         date: '31 Mar 2024',
-        button: 'www.google.com',
+        button: '#',
         prize: "₹1 lakh",
 
     },
@@ -94,8 +100,8 @@ function Modal(props) {
                                 </p>
                             </div>
                             <div className="p-4 md:p-5 ">
-                                <a  href="#" target='_blank'><motion.button disabled  onClick={props.closeModal} whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} transition={{ duration: 0.2 }} className='mb-2 md:mb-0  w-full text-lg py-1 -rounded-[2em] rounded-lg text-white bg-[#1F1401] disabled:bg-[#7d7a74]  '>
-                                    Coming Soon...
+                                <a  href={eventData.button} target='_blank'><motion.button disabled={eventData.id !== 1}  onClick={props.closeModal} whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} transition={{ duration: 0.2 }} className={`mb-2 md:mb-0  w-full text-lg py-1 -rounded-[2em] rounded-lg text-white   ${eventData.id!= 1 ? 'bg-[#7d7a74]' : 'bg-[#1F1401]'}`}>
+                                    {eventData.id!= 1 ? <span>Coming soon..</span> : <span>Register now</span>}
                                 </motion.button></a>
                             </div>
                         </div>
